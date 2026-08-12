b065aaa445ba22931e69b30c3c023f4073d8edcc
Carlos Ferrando <cferrando2015@gmail.com>
2026-08-12 18:11:06 -0300

    chore: iniciar proyecto .NET MVC

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace inmobiliaria_Urbani_Ferrando.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
