d1ae4c2b64efb548d9abab9e9f9c18a628fb6be9
urbanijosemaria6@gmail.com <urbanijosemaria6@gmail.com>
2026-08-20 20:17:52 -0300

    feat: InquilinoServices

---
+++ Services/Interfaces/IInquilinoService.cs
@@ -0,0 +1,20 @@
+using InmobiliariaApp.Models;
+
+namespace InmobiliariaApp.Services.Interfaces
+{
+    public interface IInquilinoService
+    {
+        IList<Inquilino> ObtenerLista(int paginaNro = 1, int tamPagina = 10);
+        int ObtenerCantidad();
+
+        Inquilino? ObtenerPorId(int id);
+
+        int Alta(Inquilino i);
+
+        int Modificacion(Inquilino i);
+        
+        int Baja(int id);
+
+
+    }
+}
