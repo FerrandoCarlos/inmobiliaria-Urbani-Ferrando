5a46331e30c7dc11f482a774828059ff351263fc
Carlos Ferrando <cferrando2015@gmail.com>
2026-08-20 20:07:46 -0300

    feat: crear services de propietario

--- Services/Interfaces/IPropietarioService.cs
+++ Services/Interfaces/IPropietarioService.cs
@@ -1,1 +1,14 @@
-//Service
+using InmobiliariaApp.Models;
+
+namespace InmobiliariaApp.Services.Interfaces
+{
+    public interface IPropietarioService
+    {
+        IList<Propietario> ObtenerLista(int paginaNro = 1, int tamPagina = 10);
+        int ObtenerCantidad();
+        Propietario? ObtenerPorId(int id);
+        int Alta(Propietario propietario);
+        int Modificacion(Propietario propietario);
+        int Baja(int id);
+    }
+}
