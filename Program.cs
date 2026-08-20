9fe905b66aa278a795e93ba5d5f73518670f6762
urbanijosemaria6@gmail.com <urbanijosemaria6@gmail.com>
2026-08-20 19:50:32 -0300

    feat: cambios en appsettings y program.cs

--- Program.cs
+++ Program.cs
@@ -1,8 +1,20 @@
-var builder = WebApplication.CreateBuilder(args);
+using InmobiliariaApp.Repositories.Interfaces;
+using InmobiliariaApp.Repositories.Implementations;
+using MySqlConnector;
+//using InmobiliariaApp.Services.Interfaces;
+//using InmobiliariaApp.Services.Implementations;
 
+var builder = WebApplication.CreateBuilder(args);
+ 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-
+ 
+// Registro de repositorios y servicios (Inyección de Dependencias).
+// AddScoped: una instancia por request HTTP - correcto para trabajar
+// con conexiones a base de datos (ni Singleton ni Transient).
+builder.Services.AddScoped<IPropietarioRepository, PropietarioRepository>();
+//builder.Services.AddScoped<IPropietarioService, PropietarioService>();
+ 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -12,18 +24,18 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-
+ 
 app.UseHttpsRedirection();
 app.UseRouting();
-
+ 
 app.UseAuthorization();
-
+ 
 app.MapStaticAssets();
-
+ 
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}")
     .WithStaticAssets();
-
-
+ 
+ 
 app.Run();
